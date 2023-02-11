f0ada12267434ac6dc57773b5293fb584bd4cb99
day0r <skoledanyal@gmail.com>
2023-02-11 14:47:43 +0100

    Added: Simple error handling for when connecting to db and connection string now uses env variables instead of plaintext values

--- initializers/db.go
+++ initializers/db.go
@@ -1,11 +1,21 @@
 package initializers
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+
+
 func ConnectToDatabase() {
-	dsn := "host=localhost user=admin password=admin dbname=postgres port=9920 sslmode=disable TimeZone=Europe/Copenhagen"
+	var dsnString = "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Stockholm", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT")
+	dsn := dsnString
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("error connecting to database")
+	}
 }
