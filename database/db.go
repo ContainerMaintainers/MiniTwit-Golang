4e9109d449a2d1400fdd22d41132708da41ae3ed
day0r <skoledanyal@gmail.com>
2023-02-11 14:13:15 +0100

    Added: Simple localhost connection to postgres db

---
+++ database/db.go
@@ -0,0 +1,11 @@
+package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectToDatabase() {
+	dsn := "host=localhost user=admin password=admin dbname=postgres port=9920 sslmode=disable TimeZone=Europe/Copenhagen"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
