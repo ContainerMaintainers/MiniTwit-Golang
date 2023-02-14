3ef2ee9401d69ad502ae0409f51b8b28cdb60118
Cromski <90246462+Cromski@users.noreply.github.com>
2023-02-14 17:07:29 +0100

    added: public, username & usernameFollow, endpoints,. still missing logic

--- minitwit.go
+++ minitwit.go
@@ -13,12 +13,45 @@ func ping(c *gin.Context) {
 	})
 }
 
+func public(c *gin.Context) { //Displays the latest messages of all users
+
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+
+	/* from python project:
+	   return render_template('timeline.html', messages=query_db('''
+	       select message.*, user.* from message, user
+	       where message.flagged = 0 and message.author_id = user.user_id
+	       order by message.pub_date desc limit ?''', [PER_PAGE]))
+	*/
+}
+
+func username(c *gin.Context) { //Displays a user's tweets
+
+	username := c.Param("username") //gets the <username> from the url
+	c.JSON(200, gin.H{
+		"username": username,
+	})
+
+}
+
+func usernameFollow(c *gin.Context) { //Adds the current user as follower of the given user
+	username := c.Param("username")
+	c.JSON(200, gin.H{
+		"username": username,
+	})
+}
+
 func setupRouter() *gin.Engine {
 
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
 	router.GET("/ping", ping)
+	router.GET("/public", public)
+	router.GET("/:username", username)
+	router.POST("/:username/follow", usernameFollow)
 	return router
 
 }
