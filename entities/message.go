2eb53cbb0579be358beb721743ca3fbe30bd3681
day0r <skoledanyal@gmail.com>
2023-02-19 20:51:31 +0100

    Changed: Message struct ID fields to now have 'id' capitalised

--- entities/message.go
+++ entities/message.go
@@ -1,8 +1,8 @@
 package entities
 
 type Message struct {
-	Message_id uint   `gorm:"autoIncrement; primaryKey"`
-	Author_id  uint   `gorm:"not null"`
+	Message_ID uint   `gorm:"autoIncrement; primaryKey"`
+	Author_ID  uint   `gorm:"not null"`
 	Text       string `gorm:"not null"`
 	Pub_Date   uint   `gorm:"not null"`
 	Flagged    bool   `gorm:"not null"`
