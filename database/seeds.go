fe1db1007281c40e02644a9b772b29b8367c2677
day0r <skoledanyal@gmail.com>
2023-02-19 20:26:15 +0100

    Changed: Seed struct to now not have error as function return type and adjusted the 3 create functions to also return void

--- database/seeds.go
+++ database/seeds.go
@@ -8,88 +8,88 @@ import (
 
 type Seed struct {
 	SeedName string
-	Run func(*gorm.DB) error
+	Run func(*gorm.DB)
 }
 
 func AllSeeds() []Seed {
 	return []Seed{
 		Seed{
 			SeedName: "CreateUser1",
-			Run: func(db *gorm.DB) error {
-				return CreateUser(db, 1, "user1", "user1@gmail.com", "user1iscool")
+			Run: func(db *gorm.DB) {
+				CreateUser(db, 1, "user1", "user1@gmail.com", "user1iscool")
 			},
 		},
 		Seed{
 			SeedName: "CreateUser2",
-			Run: func(db *gorm.DB) error {
-				return CreateUser(db, 2, "user2", "user2@gmail.com", "user2iscool")
+			Run: func(db *gorm.DB) {
+				CreateUser(db, 2, "user2", "user2@gmail.com", "user2iscool")
 			},
 		},
 		Seed{
 			SeedName: "CreateUser3",
-			Run: func(db *gorm.DB) error {
-				return CreateUser(db, 3, "user3", "user3@gmail.com", "user3iscool")
+			Run: func(db *gorm.DB) {
+				CreateUser(db, 3, "user3", "user3@gmail.com", "user3iscool")
 			},
 		},
 		Seed{
 			SeedName: "CreateMessage1",
-			Run: func(db *gorm.DB) error {
-				return CreateMessage(db, 1, 1, "Hello World! From user1", 123456, false)
+			Run: func(db *gorm.DB) {
+				CreateMessage(db, 1, 1, "Hello World! From user1", 123456, false)
 			},
 		},
 		Seed{
 			SeedName: "CreateMessage2",
-			Run: func(db *gorm.DB) error {
-				return CreateMessage(db, 2, 2, "Hello World! From user2", 123456, false)
+			Run: func(db *gorm.DB) {
+				CreateMessage(db, 2, 2, "Hello World! From user2", 123456, false)
 			},
 		},
 		Seed{
 			SeedName: "CreateMessage3",
-			Run: func(db *gorm.DB) error {
-				return CreateMessage(db, 3, 3, "Hello World! From user3", 123456, false)
+			Run: func(db *gorm.DB) {
+				CreateMessage(db, 3, 3, "Hello World! From user3", 123456, false)
 			},
 		},
 		Seed{
 			SeedName: "CreateFollower1",
-			Run: func(db *gorm.DB) error {
-				return CreateFollower(db, 1, 2)
+			Run: func(db *gorm.DB) {
+				CreateFollower(db, 1, 2)
 			},
 		},
 		Seed{
 			SeedName: "CreateFollower2",
-			Run: func(db *gorm.DB) error {
-				return CreateFollower(db, 1, 3)
+			Run: func(db *gorm.DB) {
+				CreateFollower(db, 1, 3)
 			},
 		},
 		Seed{
 			SeedName: "CreateFollower3",
-			Run: func(db *gorm.DB) error {
-				return CreateFollower(db, 3, 2)
+			Run: func(db *gorm.DB) {
+				CreateFollower(db, 3, 2)
 			},
 		},
 	}
 }
 
 
-func CreateUser(db *gorm.DB, userID uint, username string, email string, pw string) error {
-	return db.Create(&entities.User{
+func CreateUser(db *gorm.DB, userID uint, username string, email string, pw string) {
+	db.Create(&entities.User{
 					User_ID: userID, 
 					Username: username, 
 					Email: email, 
-					PW_Hash: pw}).Error
+					PW_Hash: pw})
 }
 
-func CreateMessage(db *gorm.DB, message uint, author uint, text string, date uint, flagged bool) error {
-	return db.Create(&entities.Message{
+func CreateMessage(db *gorm.DB, message uint, author uint, text string, date uint, flagged bool) {
+	db.Create(&entities.Message{
 					Message_id: message,
 					Author_id: author, 
 					Text: text, 
 					Pub_Date: date, 
-					Flagged: flagged}).Error
+					Flagged: flagged})
 }
 
-func CreateFollower(db *gorm.DB, who uint, whom uint) error {
-	return db.Create(&entities.Follower{
+func CreateFollower(db *gorm.DB, who uint, whom uint) {
+	db.Create(&entities.Follower{
 					Who_ID: who,
-					Whom_ID: whom}).Error
+					Whom_ID: whom})
 }
