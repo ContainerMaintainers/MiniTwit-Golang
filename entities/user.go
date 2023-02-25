3f39ddfa8c39d163edb61c7c7337f7ae406a2cab
day0r <skoledanyal@gmail.com>
2023-02-25 16:59:17 +0100

    Added: Salting function to the User struct instead of in the minitwit file

--- entities/user.go
+++ entities/user.go
@@ -1,10 +1,23 @@
 package entities
 
-import "gorm.io/gorm"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"log"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"not null"`
 	Email    string `gorm:"not null"`
 	Password string `gorm:"not null"`
+}
+
+func Salt_pwd(pwd string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.MinCost)
+	if err != nil {
+		log.Fatal("The given password could not be hashed. ", err)
+	}
+
+	return string(hash)
 }
