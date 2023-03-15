c41f6a5e61c33774547be58efa0014ad9e6bc012
Adam Julian Hvitsted Rose <68515776+adamjhr@users.noreply.github.com>
2023-03-15 13:41:57 +0100

    Added name to docker run

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ docker pull $1/minitwit:latest
 echo "Stopping current minitwit"
 docker stop minitwit
 echo "Deploying $DOCKER_USERNAME/minitwit:latest to $PORT"
-docker run --rm -d -p $2:$2 $1/minitwit:latest
+docker run --rm -d -p $2:$2 --name minitwit $1/minitwit:latest
