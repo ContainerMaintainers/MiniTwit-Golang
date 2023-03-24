2d77f22dd0124fe1bb79d29f3b9a66757f7c5dfe
adamjhr <adam.jh.rose6@gmail.com>
2023-03-24 18:11:07 +0100

    Reformatted

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -2,15 +2,15 @@ import { uid } from 'uid';
 
 describe('Register page responds', () => {
     it('Checks if /register responds', () => {
-      cy.visit('localhost:8080/register')
+        cy.visit('localhost:8080/register')
     })
-  })
+})
 
-  describe('Register error messages', () => {
+describe('Register error messages', () => {
     it('Checks for "all fields are required" error message', () => {
-      cy.visit('localhost:8080/register')
-      cy.get('input').contains('Sign Up').click()
-      cy.contains("Error: All fields are required")
+        cy.visit('localhost:8080/register')
+        cy.get('input').contains('Sign Up').click()
+        cy.contains("Error: All fields are required")
     })
 
     it('Checks for "you have to enter a valid email address" error message', () => {
@@ -21,7 +21,7 @@ describe('Register page responds', () => {
         cy.get('input[name="password2"]').type('password')
         cy.get('input').contains('Sign Up').click()
         cy.contains("Error: You have to enter a valid email address")
-      })
+    })
 
     it('Checks for "the two passwords do not matchs" error message', () => {
         cy.visit('localhost:8080/register')
@@ -31,9 +31,9 @@ describe('Register page responds', () => {
         cy.get('input[name="password2"]').type('wrong')
         cy.get('input').contains('Sign Up').click()
         cy.contains("Error: The two passwords do not match")
-      })
+    })
 
-      it('Checks for "the username is already taken" error message', () => {
+    it('Checks for "the username is already taken" error message', () => {
         const username = uid()
         cy.visit('localhost:8080/register')
         cy.get('input[name="username"]').type(username)
@@ -50,8 +50,8 @@ describe('Register page responds', () => {
         cy.get('input').contains('Sign Up').click()
 
         cy.contains("Error: The username is already taken")
-      })
-  })
+    })
+})
 
 describe('Redirection', () => {
     it('Checks that register redirects to login page', () => {
@@ -62,5 +62,5 @@ describe('Redirection', () => {
         cy.get('input[name="password2"]').type('password')
         cy.get('input').contains('Sign Up').click()
         cy.url().should('contain', '/login')
-      })
+    })
 })
