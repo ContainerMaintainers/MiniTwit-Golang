553553c9e3e039e323e59ba47e3ffb70d84cc7fe
adamjhr <adam.jh.rose6@gmail.com>
2023-03-25 11:56:36 +0100

    test order of messages

--- cypress/e2e/message.cy.js
+++ cypress/e2e/message.cy.js
@@ -109,4 +109,37 @@ describe('Create message', () => {
 
     })
 
+    it('Check if messages are in correct order', () => {
+        const message1 = uid()
+        const message2 = uid()
+        const username = uid()
+
+        cy.visit('localhost:8080/register')
+        cy.get('input[name="username"]').type(username)
+        cy.get('input[name="email"]').type('example@mail.com')
+        cy.get('input[name="password"]').type('password')
+        cy.get('input[name="password2"]').type('password')
+        cy.get('input').contains('Sign Up').click()
+
+        cy.visit('localhost:8080/login')
+        cy.get('input[name="username"]').type(username)
+        cy.get('input[name="password"]').type('password')
+        cy.get('input').contains('Sign In').click()
+        
+        cy.visit('localhost:8080/'+username)
+
+        cy.get('input[name="text"]').type(message1)
+        cy.get('input').contains("Share").click()
+
+        cy.visit('localhost:8080/'+username)
+
+        cy.get('input[name="text"]').type(message2)
+        cy.get('input').contains("Share").click()
+
+        cy.get('.messages > li').eq(0).should('contain', message2)
+        cy.get('.messages > li').eq(1).should('contain', message1)
+
+        cy.get('a').contains('sign out').click()
+
+    })
 })
