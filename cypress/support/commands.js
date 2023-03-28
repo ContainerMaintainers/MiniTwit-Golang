df14aea0d03a77fe7714c562321e1b936782826a
adamjhr <adam.jh.rose6@gmail.com>
2023-03-28 14:06:34 +0200

    custom commands for register, login, create message

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('register', (username, email, password1, password2) => {
+    cy.visit('localhost:8080/register')
+    cy.get('input[name="username"]').type(username)
+    cy.get('input[name="email"]').type(email)
+    cy.get('input[name="password"]').type(password1)
+    cy.get('input[name="password2"]').type(password2)
+    cy.get('input').contains('Sign Up').click()
+})
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('localhost:8080/login')
+    cy.get('input[name="username"]').type(username)
+    cy.get('input[name="password"]').type(password)
+    cy.get('input').contains('Sign In').click()
+})
+
+Cypress.Commands.add('createMessage', (username, message) => {
+    cy.visit('localhost:8080/' + username)
+    cy.get('input[name="text"]').type(message)
+    cy.get('input').contains("Share").click()
+})
