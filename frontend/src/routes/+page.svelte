aa59a6e265734841515d8a7395bb645357704146
Cromski <jakobrhenriksen@outlook.com>
2023-03-28 11:47:28 +0200

    setup sveltekit project with tailwindcss

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    
+</script>
+
+<h1 class=" bg-green-700 font-bold" >Hello there</h1>
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{svelte,html,ts,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
