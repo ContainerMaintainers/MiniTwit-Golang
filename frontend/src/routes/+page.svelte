b771148d8c2e8ee85018b8468bd54d13a87cedb7
Cromski <jakobrhenriksen@outlook.com>
2023-03-29 08:20:05 +0200

    first api request to show data

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,8 +2,19 @@
     
     //let data = fetch('localhost:8080/').then(res => res.json())
 
-    fetch('http://localhost:8080/public').then(res => res.json()).then(data => console.log(data))
-
+    let apiResJson = fetch('http://localhost:8080/public').then(res => res.json())
+    let data = apiResJson.then(res => res['messages'])
 </script>
 
-<h1 class=" bg-green-700 font-bold" >asdf</h1>
+<h1 class=" bg-green-700 font-bold" >tweets:</h1>
+{#await data}
+    <div class="bg-green-200">
+        <h1 class="font-bold">Loading...</h1>
+    </div>
+{:then data}
+{#each  data as tweet}
+    <div class="bg-green-200">
+        <h1 class="font-bold">{tweet.Text}</h1>
+    </div>
+{/each}
+{/await}
