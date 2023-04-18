d88972ecab048b1e780be5da192f3a8ef495a727
Day0r <90214655+Day0r@users.noreply.github.com>
2023-04-18 16:57:41 +0200

    Changed: DB_PASS env variable to DB_PASSWORD

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ docker pull $1/minitwit:latest
 echo "Stopping current minitwit"
 docker stop minitwit
 echo "Deploying $DOCKER_USERNAME/minitwit:latest to $PORT"
-docker run --rm -d -p $2:$2 --name minitwit $1/minitwit:latest -e DB_NAME=$3 -e DB_USER=$6 -e DB_PASS=$5 -e DB_HOST=$7 -e DB_PORT=$4 -e PORT=$2
+docker run --rm -d -p $2:$2 --name minitwit $1/minitwit:latest -e DB_NAME=$3 -e DB_USER=$6 -e DB_PASSWORD=$5 -e DB_HOST=$7 -e DB_PORT=$4 -e PORT=$2
