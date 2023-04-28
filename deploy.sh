74d42e01e19e8fbbc1489073020e28758d158a76
adamjhr <adam.jh.rose6@gmail.com>
2023-04-28 15:21:16 +0200

    Updated deploy script to include connecting to loki

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,35 @@
 #!/usr/bin/env bash
 
+# ARGUMENTS
+# $1 : Docker hub username
+# $2 : Application port 
+# $3 : Database name
+# $4 : Database port
+# $5 : Database password
+# $6 : Database user
+# $7 : Database host
+# $8 : Loki username
+# $9 : Loki password
+# $10 : Loki host
+# $11 : Loki port
+
 echo "Pulling latest version"
 docker pull $1/minitwit:latest
 echo "Stopping current minitwit"
 docker stop minitwit
 echo "Deploying $DOCKER_USERNAME/minitwit:latest to $PORT"
-docker run --rm -d -p $2:$2 --name minitwit $1/minitwit:latest
+docker run -d \
+	-p $2:$2 \
+    --env PORT=$2 \
+	--env DB_NAME=$3 \
+    --env DB_PORT=$4 \
+    --env DB_PASSWORD=$5 \
+	--env DB_USER=$6 \
+	--env DB_HOST=$7 \
+	--name minitwit \
+	--log-driver=loki \
+    --log-opt loki-url="https://${8}:${9}@${10}:${11}/loki/api/v1/push" \
+    --log-opt loki-retries=5 \
+    --log-opt loki-batch-size=400 \
+    --name minitwit \
+	$1/minitwit
